8728d9a38a166bcf572ab1149579cdf4fcdf7715
Navarra Manuel <manusky@hotmail.fr>
2016-10-14 12:53:43 +0200

    Commented file

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Updating SQL connections in inifile from Environment
 sed -i -e "s/host=127.0.0.1/host=$TS_DB_HOST/g" /opt/teamspeak/ts3db_sql.ini
 sed -i -e "s/port=3306/port=$TS_DB_PORT/g" /opt/teamspeak/ts3db_sql.ini
 sed -i -e "s/username=teamspeak/username=$TS_DB_USER/g" /opt/teamspeak/ts3db_sql.ini
 sed -i -e "s/password=password/password=$TS_DB_PASSWORD/g" /opt/teamspeak/ts3db_sql.ini
 sed -i -e "s/database=teamspeak/database=$TS_DB_NAME/g" /opt/teamspeak/ts3db_sql.ini
 
+# Running server
 /opt/teamspeak/ts3server_minimal_runscript.sh inifile=/opt/teamspeak/ts3server.ini
