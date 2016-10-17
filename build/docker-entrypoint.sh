e7c17de447b749a19d55a9ea7d45ce55b6d99fe7
Navarra Manuel <manusky@hotmail.fr>
2016-10-17 12:29:16 +0200

    Check if SQL Server is available before start

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+set -e
+
+until mysql -h"$TS_DB_HOST" -P"$TS_DB_PORT" -u"$TS_DB_USER" -p"$TS_DB_PASSWORD" -e "use $TS_DB_NAME"; do
+    >&2 echo "SQL is unavailable - sleeping"
+    sleep 3
+done
+
 # Updating SQL connections in inifile from Environment
 sed -i -e "s/host=127.0.0.1/host=$TS_DB_HOST/g" /opt/teamspeak/ts3db_sql.ini
 sed -i -e "s/port=3306/port=$TS_DB_PORT/g" /opt/teamspeak/ts3db_sql.ini
